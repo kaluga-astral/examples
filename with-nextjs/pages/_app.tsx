14c74e3bc3f8d40081d7732f91118ad205fd7d42
Perevezentsev Pavel <perevezenzev@gmail.com>
2022-02-09 15:51:54 +0300

    Initializing

---
+++ with-nextjs/pages/_app.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@astral/ui'
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
+export function App(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default App
---
+++ with-nextjs/src/theme.js
@@ -0,0 +1,5 @@
+import { createTheme } from '@astral/ui';
+
+export const theme = createTheme();
+
+export default theme;
