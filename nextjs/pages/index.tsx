d7ffa7a162a23921096fa03d076b42f7f4500236
Perevezentsev Pavel <perevezenzev@gmail.com>
2022-06-10 16:08:25 +0300

    Добавляет use-cases

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,18 +1,15 @@
 import { NextPage } from 'next';
-import { TextField, Typography, Button } from '@astral/ui';
+import { TextField, Typography, Button, Tooltip, CircularProgress } from '@astral/ui';
 
 export const IndexPage: NextPage = () => {
   return (
     <div>
       <Typography>IndePage</Typography>
-      <Button>click me</Button>
-      {new Array(500).fill('').map((_value, i) => {
-        return (
-          <div key={i}>
-            <TextField label="With placeholder" placeholder="Placeholder value" />
-          </div>
-        )
-      })}
+      <Tooltip title="click me">
+        <Button>click me</Button>
+      </Tooltip>
+      <CircularProgress color='primary' size='medium' />
+      <TextField label="With placeholder" placeholder="Placeholder value" />
     </div>
   );
 }
